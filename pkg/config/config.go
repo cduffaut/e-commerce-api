c54fb33f49550d7ddd0cf5110db983bcf599879e
cduffaut <cecileduffaut21@gmail.com>
2026-06-09 22:24:57 +0200

    feat(config): add load env function

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,9 +1,37 @@
 package config
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
 	Port               string
 	DatabaseURL        string
 	JWTSecret          string
 	StripeSecretKey    string
 	StripWebhookSecret string
+}
+
+func Load() *Config {
+	_ = godotenv.Load()
+
+	cfg := &Config{
+		Port:               getEnv("PORT", "8080"),
+		DatabaseURL:        getEnv("DATABASE_URL", ""),
+		JWTSecret:          getEnv("JWT_SECRET", ""),
+		StripeSecretKey:    getEnv("STRIPE_SECRET_KEY", ""),
+		StripWebhookSecret: getEnv("STRIPE_WEBHOOK_SECRET", ""),
+	}
+
+	if cfg.DatabaseURL == "" {
+		log.Fatal("DATABASE_URL URL is required")
+	}
+
+	if cfg.JWTSecret == "" {
+		log.Fatal("JWT_SECRET is required")
+	}
+
+	return cfg
 }
