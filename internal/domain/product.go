d5a29531afbba7bc38b3ef746d27a04e1552bf1e
cduffaut <cecileduffaut21@gmail.com>
2026-06-15 23:39:28 +0200

    feat(product): add structs product related

---
+++ internal/domain/product.go
@@ -0,0 +1,41 @@
+package domain
+
+import "time"
+
+type Product struct {
+	ID          int64     `json:"db:id"`
+	Name        string    `json:"db:name"`
+	Description string    `json:"db:description"`
+	Price       float64   `json:"db:price"`
+	Stock       int       `json:"db:stock"`
+	Category    string    `json:"db:category"`
+	IsActive    bool      `json:"db:is_active"`
+	CreatedAt   time.Time `json:"db:created_at"`
+	UpdatedAt   time.Time `json:"db:updated_at"`
+}
+
+// resered to admin
+type CreateProductRequest struct {
+	Name        string  `json:"name" binding:"required"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price" binding:"omitempty,gt=0"`
+	Stock       int     `json:"stock" binding:"omitempty,gt=0"`
+	Category    string  `json:"category"`
+}
+
+type UpdateProductRequest struct {
+	Name        *string  `json:"name"`
+	Description *string  `json:"description"`
+	Price       *float64 `json:"price" binding:"omitempty, gt=0"`
+	Stock       *int     `json:"stock" binding:"omitempty, gte=0"`
+	Category    *string  `json:"category"`
+	IsActive    *bool    `json:"is_active"`
+}
+
+// parameters for filters/research
+type ProductFilter struct {
+	Search   string `form:"q"`
+	Category string `form:"category"`
+	Page     int    `form:"page,default=1"`
+	PageSize int    `form:"page_size,default=20"`
+}
