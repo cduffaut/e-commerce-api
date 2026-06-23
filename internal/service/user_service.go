f8a697eb104404ce119f0c54ae5deb3ed4fb2196
cduffaut <cecileduffaut21@gmail.com>
2026-06-23 23:55:23 +0200

    feat(service): add user service functions/struct/interface

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,1 +1,111 @@
 package service
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/cduffaut/e-commerce-api/internal/domain"
+	"github.com/cduffaut/e-commerce-api/internal/repository"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserService interface {
+	Register(ctx context.Context, req domain.CreateUserRequest) (*domain.AuthResponse, error)
+	Login(ctx context.Context, req domain.LoginRequest) (*domain.AuthResponse, error)
+	GetByID(ctx context.Context, id int64) (*domain.UserResponse, error)
+}
+
+type userService struct {
+	repo      repository.UserRepository
+	jwtSecret string
+}
+
+func newUserService(repo repository.UserRepository, jwtSecret string) UserService {
+	return &userService{repo: repo, jwtSecret: jwtSecret}
+}
+
+func (s *userService) Register(ctx context.Context, req domain.CreateUserRequest) (*domain.AuthResponse, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), 12)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	user := &domain.User{
+		Email:    req.Email,
+		Password: string(hashedPassword),
+		Role:     "user",
+	}
+
+	if err := s.repo.Create(ctx, user); err != nil {
+		return nil, fmt.Errorf("failed to create user: %w", err)
+	}
+
+	token, err := s.generateToken(user)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate toke: %w", err)
+	}
+
+	return &domain.AuthResponse{
+		Token: token,
+		User: domain.UserResponse{
+			ID:    user.ID,
+			Email: user.Email,
+			Role:  user.Role,
+		},
+	}, nil
+}
+
+func (s *userService) Login(ctx context.Context, req domain.LoginRequest) (*domain.AuthResponse, error) {
+	user, err := s.repo.GetByEmail(ctx, req.Email)
+	if err != nil {
+		return nil, fmt.Errorf("invalid credentials")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		return nil, fmt.Errorf("invalid credentials")
+	}
+
+	token, err := s.generateToken(user)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate token: %w", err)
+	}
+
+	return &domain.AuthResponse{
+		Token: token,
+		User: domain.UserResponse{
+			ID:    user.ID,
+			Email: user.Email,
+			Role:  user.Role,
+		},
+	}, nil
+}
+
+func (s *userService) GetByID(ctx context.Context, id int64) (*domain.UserResponse, error) {
+	user, err := s.repo.GetByID(ctx, id)
+
+	if err != nil {
+		return nil, fmt.Errorf("user not found: %w", err)
+	}
+
+	return &domain.UserResponse{
+		ID:    user.ID,
+		Email: user.Email,
+		Role:  user.Role,
+	}, nil
+}
+
+func (s *userService) generateToken(user *domain.User) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": user.ID,
+		"role":    user.Role,
+		"exp":     jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(s.jwtSecret))
+}
