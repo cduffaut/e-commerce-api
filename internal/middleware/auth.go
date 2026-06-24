4046cb2cab2636e5456a8bb393bb34528e15a783
cduffaut <cecileduffaut21@gmail.com>
2026-06-24 23:12:36 +0200

    feat(middleware): auth middleware functions created

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,1 +1,105 @@
 package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const (
+	UserIDKey = "user_id"
+	RoleKey   = "role"
+)
+
+func AuthMiddleware(jwtSecret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// reading header Authorization
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "authorization header required",
+			})
+			return
+		}
+		// check bearer format
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "authorization header format must be: Bearer <token>",
+			})
+			return
+		}
+
+		tokenString := parts[1]
+
+		// parse & validate the token
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			// check if algorithm is HS256
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(jwtSecret), nil
+		})
+
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid or expired token",
+			})
+			return
+		}
+
+		// extracting claims
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid token claims",
+			})
+			return
+		}
+		c.Set(UserIDKey, claims["user_id"])
+		c.Set(RoleKey, claims["role"])
+
+		c.Next()
+	}
+}
+
+// check if user has Admin role
+func AdminMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		role, exists := c.Get(RoleKey)
+		if !exists {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "unauthorized",
+			})
+			return
+		}
+
+		if role != "admin" {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
+				"error": "admin access requiered",
+			})
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func GetUserID(c *gin.Context) (int64, bool) {
+	val, exists := c.Get(UserIDKey)
+
+	if !exists {
+		return 0, false
+	}
+
+	userIDFloat, ok := val.(float64)
+
+	if !ok {
+		return 0, false
+	}
+
+	return int64(userIDFloat), true
+}
